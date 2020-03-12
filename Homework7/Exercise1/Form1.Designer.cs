f000ee937a3d61ec248dd970299942c51a030fe4
Irina <irinatyumyantseva@gmail.com>
2020-03-12 14:12:46 +0300

    seventh commit

---
+++ Homework7/Exercise1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonToGetNumber = new System.Windows.Forms.Button();
+            this.gettingNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonToGetNumber
+            // 
+            this.buttonToGetNumber.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonToGetNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonToGetNumber.Location = new System.Drawing.Point(95, 271);
+            this.buttonToGetNumber.Name = "buttonToGetNumber";
+            this.buttonToGetNumber.Size = new System.Drawing.Size(150, 40);
+            this.buttonToGetNumber.TabIndex = 0;
+            this.buttonToGetNumber.Text = "Проверить решение";
+            this.buttonToGetNumber.UseVisualStyleBackColor = false;
+            this.buttonToGetNumber.Click += new System.EventHandler(this.buttonToGetNumber_Click);
+            // 
+            // gettingNumber
+            // 
+            this.gettingNumber.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.gettingNumber.Location = new System.Drawing.Point(96, 222);
+            this.gettingNumber.Name = "gettingNumber";
+            this.gettingNumber.Size = new System.Drawing.Size(149, 22);
+            this.gettingNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(95, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Угадайте число от 1 до 100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(266, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "< Ответ вводить сюда";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gettingNumber);
+            this.Controls.Add(this.buttonToGetNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonToGetNumber;
+        private System.Windows.Forms.TextBox gettingNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Homework7/Exercise2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool firstCase = false;
+        bool secondCase = false;
+        bool thirdCase = false;
+        bool fourthCase = false;
+        bool fifthCase = false;
+        bool sixthCase = false;
+        bool seventhCase = false;
+        bool eighthCase = false;
+
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+
+
+        private void Button1_1_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase)
+            {
+                secondCase = true;
+                button1_1.BackColor = Color.Brown;
+                button1_1.Text = "2";
+            }
+        }
+
+        private void Button1_2_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase && secondCase && thirdCase && fourthCase && fifthCase && sixthCase)
+            {
+                seventhCase = true;
+                button1_2.BackColor = Color.Brown;
+                button1_2.Text = "7";
+            }
+        }
+
+        private void Button1_3_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase && secondCase)
+            {
+                thirdCase = true;
+                button1_3.BackColor = Color.Brown;
+                button1_3.Text = "3";
+            }
+        }
+
+        private void Button2_1_Mouse(object sender, EventArgs e)
+        {
+            if (firstCase && secondCase && thirdCase && fourthCase && fifthCase && sixthCase && seventhCase && eighthCase)
+            {
+                button2_1.BackColor = Color.Brown;
+                button2_1.Text = "9";
+            }
+        }
+
+        private void Button2_2_Mouse(object sender, EventArgs e)
+        {
+            firstCase = true;
+            button2_2.BackColor = Color.Brown;
+            button2_2.Text = "1";
+        }
+
+        private void Button2_3_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase && secondCase && thirdCase && fourthCase && fifthCase)
+            {
+                sixthCase = true;
+                button2_3.BackColor = Color.Brown;
+                button2_3.Text = "6";
+            }
+        }
+
+        private void Button3_1_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase && secondCase && thirdCase && fourthCase && fifthCase && sixthCase && seventhCase)
+            {
+                eighthCase = true;
+                button3_1.BackColor = Color.Brown;
+                button3_1.Text = "8";
+            }
+        }
+
+        private void Button3_2_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase && secondCase && thirdCase)
+            {
+                fourthCase = true;
+                button3_2.BackColor = Color.Brown;
+                button3_2.Text = "4";
+            }
+        }
+
+        private void Button3_3_Mouse(object sender, EventArgs e)
+        {
+            
+            if (firstCase && secondCase && thirdCase && fourthCase)
+            {
+                fifthCase = true;
+                button3_3.BackColor = Color.Brown;
+                button3_3.Text = "5";
+            }
+        }
+    }
+}
