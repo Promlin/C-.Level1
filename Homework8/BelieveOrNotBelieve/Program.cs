a3b2a18dbcc378527a7a6b3ac03c36023a2101d6
Irina <irinatyumyantseva@gmail.com>
2020-03-15 11:20:06 +0300

    eighth commit

---
+++ Homework8/BelieveOrNotBelieve/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace BelieveOrNotBelieve
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    // Класс для вопроса    
+    [Serializable]
+    public class Question
+    {
+        public string text;       
+        public bool trueFalse;
+                              
+        public Question()
+        {
+        }
+        public Question(string text, bool trueFalse)
+        {
+            this.text = text;
+            this.trueFalse = trueFalse;
+        }
+    }
+    // Класс для хранения списка вопросов. А также для сериализации в XML и десериализации из XML
+    class TrueFalse
+    {
+        string fileName;
+        List<Question> list;
+        public string FileName
+        {
+            set { fileName = value; }
+        }
+        public TrueFalse(string fileName)
+        {
+            this.fileName = fileName;
+            list = new List<Question>();
+        }
+        public void Add(string text, bool trueFalse)
+        {
+            list.Add(new Question(text, trueFalse));
+        }
+        public void Remove(int index)
+        {
+            if (list != null && index < list.Count && index >= 0) list.RemoveAt(index);
+        }
+        // Индексатор - свойство для доступа к закрытому объекту
+        public Question this[int index]
+        {
+            get { return list[index]; }
+        }
+        public void Save()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, list);
+            fStream.Close();
+        }
+        public void Load()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            list = (List<Question>)xmlFormat.Deserialize(fStream);
+            fStream.Close();
+        }
+        public int Count
+        {
+            get { return list.Count; }
+        }
+    }
+}
